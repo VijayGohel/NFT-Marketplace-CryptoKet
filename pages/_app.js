7cdb17d104d8a6c91d644239bc9368bffea0651c
vijaygohel <vijaygohel5454@gmail.com>
2023-10-29 21:22:57 +0530

    add ThemeProvider & fontawesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
+import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
+
 import '../styles/globals.css';
 import { Navbar, Footer } from '../components';
 
 const MyApp = ({ Component, pageProps }) => (
-  <div className="dark:bg-nft-dark bg-white min-h-screen">
-    <Navbar />
-    <Component {...pageProps} />
-    <Footer />
-  </div>
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+
+    <Script src="https://kit.fontawesome.com/5946e66caa.js" crossorigin="anonymous" />
+  </ThemeProvider>
 );
 
 export default MyApp;
