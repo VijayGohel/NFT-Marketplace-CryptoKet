c1de884af8a2968b4c3b1c91ac746f5848f21ea4
vijaygohel <vijaygohel5454@gmail.com>
2023-10-30 01:07:12 +0530

    add theme toggle in navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,25 +1,46 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
 import images from '../assets';
 
-const Navbar = () => (
-  <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 boder-nft-gray-1">
-    <div className="flex flex-1 flex-row justify-start">
-      <Link href="/">
-        <div className="flexCenter cursor-pointer md:hidden">
-          <Image src={images.logo02} width={32} height={32} alt="brand logo" />
-          <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">Cryptoket</p>
+const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 boder-nft-gray-1">
+      <div className="flex flex-1 flex-row justify-start">
+        <Link href="/">
+          <div className="flexCenter cursor-pointer md:hidden">
+            <Image src={images.logo02} width={32} height={32} alt="brand logo" />
+            <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">Cryptoket</p>
+          </div>
+        </Link>
+        <Link href="/">
+          <div className="hidden md:flex">
+            <Image src={images.logo02} width={32} height={32} alt="brand logo" />
+          </div>
+        </Link>
+      </div>
+
+      <div className="flex flex-initial flex-row justify-end">
+        <div className="flex items-center mr-2">
+          <input
+            type="checkbox"
+            id="theme-toggle"
+            className="checkbox"
+            onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          />
+          <label htmlFor="theme-toggle" className="flexBetween w-8 h-4 bg-black rounded-2xl p-1 relative label">
+            <i className="fas fa-sun" />
+            <i className="fas fa-moon" />
+            <div className="w-3 h-3 absolute bg-white rounded-full ball" />
+          </label>
         </div>
-      </Link>
-      <Link href="/">
-        <div className="hidden md:flex">
-          <Image src={images.logo02} width={32} height={32} alt="brand logo" />
-        </div>
-      </Link>
-    </div>
-  </nav>
-);
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
