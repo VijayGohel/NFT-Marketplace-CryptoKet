b7aa0fa66c93cc69f2ba4cc8f4f225fe512063f7
vijaygohel <vijaygohel5454@gmail.com>
2023-11-01 00:00:23 +0530

    add menu items in navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
 
 import images from '../assets';
 
+const navLinks = {
+  'Explore NFTs': '/',
+  'Listed NFTs': '/created-nfts',
+  'My NFTs': '/my-nfts',
+};
+
+const MenuItems = ({ isMobile, active, setActive }) => (
+  <ul className={`list-none flexCenter flex-row ${isMobile && 'flex-col h-full'}`}>
+    {Object.keys(navLinks).map((item, index) => (
+      <li
+        onClick={() => setActive(item)}
+        key={`${item} ${index}`}
+        className={`flex flex-row items-center font-poppins font-semibold text-base dark:hover:text-white hover:text-nft-dark mx-3 ${active === item ? 'dark:text-white text-nft-black-1' : 'dark:text-nft-gray-3 text-nft-gray-2'}`}
+      >
+        <Link href={navLinks[item]}>{item}</Link>
+      </li>
+    ))}
+  </ul>
+);
+
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
+  const [activeLink, setActiveLink] = useState('Explore NFTs');
 
   return (
     <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 boder-nft-gray-1">
@@ -37,6 +58,9 @@ const Navbar = () => {
             <i className="fas fa-moon" />
             <div className="w-3 h-3 absolute bg-white rounded-full ball" />
           </label>
+        </div>
+        <div className="flex md:hidden">
+          <MenuItems active={activeLink} setActive={setActiveLink} />
         </div>
       </div>
     </nav>
