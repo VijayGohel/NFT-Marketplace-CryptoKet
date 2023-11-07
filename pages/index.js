17e2a770e3a095ef1876910f60adb7d15156a2c8
vijaygohel <vijaygohel5454@gmail.com>
2023-11-08 01:07:16 +0530

    add scroll arrows

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import React, { useRef } from 'react';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
 import Images from '../assets';
 import { makeId } from '../utils/makeId';
@@ -7,6 +9,7 @@ import { Banner, CreatorCard } from '../components';
 const Home = () => {
   const parentRef = useRef(null);
   const scrollRef = useRef(null);
+  const { theme } = useTheme();
   return (
     <div className="flex justify-center sm:px-4 p-12">
       <div className="w-full minmd:w-4/5">
@@ -22,16 +25,34 @@ const Home = () => {
           <div className="relative flex-1 max-w-full flex-mt-3" ref={parentRef}>
             <div className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none" ref={scrollRef}>
               {
-              [6, 7, 8, 9, 10].map((i) => (
-                <CreatorCard
-                  key={`creator-${i}`}
-                  rank={i}
-                  creatorImage={Images[`creator${i}`]}
-                  creatorName={`0x${makeId(3)}...${makeId(4)}`}
-                  creatorEths={10 - i * 0.5}
+                [6, 7, 8, 9, 10].map((i) => (
+                  <CreatorCard
+                    key={`creator-${i}`}
+                    rank={i}
+                    creatorImage={Images[`creator${i}`]}
+                    creatorName={`0x${makeId(3)}...${makeId(4)}`}
+                    creatorEths={10 - i * 0.5}
+                  />
+                ))
+              }
+              <div className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 left-0 cursor-pointer">
+                <Image
+                  src={Images.left}
+                  layout="fill"
+                  objectFit="contain"
+                  alt="left-arrow"
+                  className={theme === 'light' && 'invert filter'}
                 />
-              ))
-            }
+              </div>
+              <div className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 right-0 cursor-pointer">
+                <Image
+                  src={Images.right}
+                  layout="fill"
+                  objectFit="contain"
+                  alt="left-arrow"
+                  className={theme === 'light' && 'invert filter'}
+                />
+              </div>
             </div>
           </div>
         </div>
