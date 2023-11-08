c7d9961250c6d364b050f112f6eec391ab69d1c8
vijaygohel <vijaygohel5454@gmail.com>
2023-11-09 01:11:30 +0530

    add styles to drop zone

--- pages/create-nft.js
+++ pages/create-nft.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
+import { useDropzone } from 'react-dropzone';
 
 import Images from '../assets';
 
 const CreateNFT = () => {
   const { theme } = useTheme();
+  const [fileUrl, setFileUrl] = useState(null);
+
+  const onDrop = useCallback(() => {
+
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } = useDropzone({ onDrop, accept: 'image/*', maxSize: 5000000 });
+
+  const fileStyle = useMemo(() => (
+    `dark:bg-nft-black-1 bg-white border dark:border-white border-nft-gray-2 flex flex-col items-center p-5 rounded-sm border-dashed
+    ${isDragAccept && ' border-file-accept'}
+    ${isDragActive && ' border-file-active'}
+    ${isDragReject && ' border-file-reject'}`
+  ), [isDragAccept, isDragActive, isDragReject]);
+
   return (
     <div className="flex justify-center sm:px-4 p-12">
       <div className="w-3/5 md:w-full">
@@ -16,8 +32,8 @@ const CreateNFT = () => {
         <div className="mt-16">
           <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl">Upload File</p>
           <div className="mt-4">
-            <div>
-              <input />
+            <div {...getRootProps()} className={fileStyle}>
+              <input {...getInputProps()} />
               <div className="flexCenter flex-col text-center">
                 <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl">JPG, PNG, GIF, SVG, WEBM Max 100mb.</p>
 
