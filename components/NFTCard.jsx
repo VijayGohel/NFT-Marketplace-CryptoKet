8482ad6b93247650dd9fcc30236ed010d207ab04
vijaygohel <vijaygohel5454@gmail.com>
2023-11-17 23:28:50 +0530

    add default image for NFT

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -14,10 +14,10 @@ const NFTCard = ({ nft }) => {
       <div className="flex-1 min-w-215 max-w-max xs:max-w-none sm:w-full sm:min-w-155 minmd:min-w-256 minlg:min-w-327 dark:bg-nft-black-3 bg-white rounded-2xl p-4 m-4 minlg:m-8 sm:my-2 sm:mx-2 cursor-pointer shadow-md">
         <div className="relative w-full h-52 sm:h-36 xs:h-56 minmd:h-60 minlg:h-300 rounded-2xl overflow-hidden">
           <Image
-            src={nft.image || Images[`nft${nft.i}`]}
+            src={nft.image || Images['upload.png']}
             layout="fill"
             objectFit="cover"
-            alt={`nft${nft.i}`}
+            alt={`${nft.name} image`}
           />
         </div>
         <div className="mt-3 flex flex-col font-poppins dark:text-white text-nft-black-1 font-semibold text-xs minlg:text-lg">
